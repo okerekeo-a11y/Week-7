98848b50c83342ea9d2cda2a2867ad268de5478f
okerekeo-a11y <okerekeo@students.trinitydc.edu>
2026-04-08 18:07:47 -0400

    Add program to merge dictionaries

---
+++ merge_dictionaries.py
@@ -0,0 +1,11 @@
+"""
+Ogechukwu Okereke
+CSMC 111
+Spring 2026
+week7 assignment4
+"""
+dict1 = {"a": 1, "b": 2}
+dict2 = {"b": 3, "c": 4}
+merged_dict = dict1.copy()
+merged_dict.update(dict2)
+print(merged_dict)
